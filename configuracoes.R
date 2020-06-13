2d62aed49f8e0e0bc5449457e784b68ac50196d3
Beatriz Vianna <bia.vianna@gmail.com>
2020-06-13 18:01:14 -0300

    new_profile feature

--- configuracoes.R
+++ configuracoes.R
@@ -1,3 +1,8 @@
+#carregar pacotes
 library(googlesheets4)
-read_sheet("https://docs.google.com/spreadsheets/d/1t3868phzUgR2Q_8k_wQpYMM7G-Nc61xXpdV4TMVPbB0/edit#gid=0")
+
+
+#conectar ao sheets
+base <- read_sheet("https://docs.google.com/spreadsheets/d/1t3868phzUgR2Q_8k_wQpYMM7G-Nc61xXpdV4TMVPbB0/edit#gid=0")
+
 
